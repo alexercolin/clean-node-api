c1cde884f01c0c5495c2f79acf5d4ab926eef76f
alexercolin <alex_ercolin@hotmail.com>
2021-12-01 19:18:38 -0300

    test: Add tests CompareFieldValidation

---
+++ src/presentation/helpers/validators/compare-field-validation.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+describe('Compare Field validation', () => {
+  test('should return a InvalidParamError if validation fails', () => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'wrong_value'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(error).toBeFalsy()
+  })
+})
