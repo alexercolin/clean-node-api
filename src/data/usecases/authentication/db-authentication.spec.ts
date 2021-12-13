736c593c7c7d6c0bfb5b5743a6537cdc001355ff
alexercolin <alex_ercolin@hotmail.com>
2021-12-13 20:18:57 -0300

    test: Ensure LoadAccountByEmailRespository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -52,4 +52,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRespository returns null', async () => {
+    const { sut, loadAccountByEmailRespositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRespositoryStub, 'load').mockReturnValue(null)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
