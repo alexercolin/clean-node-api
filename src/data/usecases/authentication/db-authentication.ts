b908b289a7db3593a2e25d20a0a23621cda54486
alexercolin <alex_ercolin@hotmail.com>
2021-12-18 14:34:53 -0300

    feat: Should call UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,15 +2,18 @@ import { Authentication, AuthenticationModel } from '../../../domain/usecases/au
 import { HashCompare } from '../../protocols/criptography/hash-compare'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRespository } from '../../protocols/db/load-account--by-email-respository'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRespository: LoadAccountByEmailRespository
   private readonly hashCompare: HashCompare
   private readonly tokenGenerator: TokenGenerator
-  constructor (loadAccountByEmailRespository: LoadAccountByEmailRespository, hashCompare: HashCompare, tokenGenerator: TokenGenerator) {
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
+  constructor (loadAccountByEmailRespository: LoadAccountByEmailRespository, hashCompare: HashCompare, tokenGenerator: TokenGenerator, updateAccessTokenRepository: UpdateAccessTokenRepository) {
     this.loadAccountByEmailRespository = loadAccountByEmailRespository
     this.hashCompare = hashCompare
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -19,6 +22,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashCompare.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
