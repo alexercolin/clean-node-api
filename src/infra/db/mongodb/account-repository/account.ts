9727716b5c04cf85fe3b26dd0c500b264523c470
alexercolin <alex_ercolin@hotmail.com>
2021-12-23 07:16:34 -0300

    feat: Ensure loadBy returns null if fails

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -18,6 +18,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadByEmail (email: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email })
-    return MongoHelper.map(account)
+
+    return account && MongoHelper.map(account)
   }
 }
