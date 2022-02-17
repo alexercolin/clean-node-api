87225c7cb5b5dcfd463c36ea85dbaa3348028c13
alexercolin <alex_ercolin@hotmail.com>
2022-02-16 22:20:28 -0300

    fix: remove jest error

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -15,7 +15,6 @@ export const MongoHelper = {
   },
 
   async getCollection (name: string): Promise<Collection> {
-    await this.connect(this.uri)
     return this.client.db().collection(name)
   },
 
