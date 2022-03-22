6f19270f5856db20a246a71deeab5efe4cf26f4d
aoliveira <aoliveira@BRM0065.local>
2022-03-21 23:15:39 -0300

    feat: ensure SaveSurveyResult Controller returns forbidden if surveyId is not found

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,10 +1,15 @@
 import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../login/login-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    const surveyId = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+
+    if (!surveyId) return forbidden(new InvalidParamError('surveyId'))
+
     return null
   };
 }
