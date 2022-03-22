b4d4edba74dbe58937c45057a0e18e0c7cf3b3b7
aoliveira <aoliveira@BRM0065.local>
2022-03-22 11:31:23 -0300

    feat: Ensure returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,15 +1,19 @@
 import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../login/login-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveyId = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    try {
+      const surveyId = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
 
-    if (!surveyId) return forbidden(new InvalidParamError('surveyId'))
+      if (!surveyId) return forbidden(new InvalidParamError('surveyId'))
 
-    return null
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   };
 }
